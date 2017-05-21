3ca4f68ce2ab497a3585faaaa334d71eac447c9d
Matthew Meyn <meynm@flip1.engr.oregonstate.edu>
2017-05-20 17:48:10 -0700

    constant delta smoothing

--- train-smooth-ngram.py
+++ train-smooth-ngram.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 import sys
 import random
 import string
@@ -18,7 +20,6 @@ def Train(filename, order = 3, lid = 0.00001):
     data = data.replace(' ', '_')
     data = data.replace('\n', '$')
     chars = set([c for c in data])# if c != '\n'])
-    #chars.add('*')
     data = string.lower(data)
     data = (order - 1) * '#' + data
 
@@ -49,23 +50,14 @@ def Train(filename, order = 3, lid = 0.00001):
 
     return ngram
 
-    #model = {}
-    #for seq, char_count in ngram.items():
-    #    n = sum(char_count.values()) + count * lid
-    #    if seq not in model:
-    #        model[seq] = {}
-    #    for char, count in char_count.items():
-    #        model[seq][char] = float(count) / n
-    #return model
-
 
 def MakeFSA(ngram, order, startSymbol = '<s>'):
-    fsa = 'F\n(F (F *e* 1.0))'
+    fsa = 'F\n(S (S *e* 1.0))'
     for seq, char_prob in ngram.items():
         for char, prob in char_prob.items():
             # if all chars are start symbol
             if seq[-1] == '#':
-                fsa += '(F ({0} {1} {2}))\n'.format(
+                fsa += '(S ({0} {1} {2}))\n'.format(
                     seq[1 :] + char, startSymbol, prob
                 )
             else:
@@ -78,50 +70,12 @@ def MakeFSA(ngram, order, startSymbol = '<s>'):
                         seq, seq[1 :] + char, char, prob
                     )
     return fsa
-
+   
     
-
-
-    
-    #for seq, lp in ngram.items():
-    #    for letter, prob in lp.items():
-    #        fsa += '(s{0} ({1} {2} {3}))\n'.format(
-    #            seq, seq,
-    #        )
-
-
-def generate_letter(ngram, history, order):
-    history = history[-(order - 1) : ]
-    dist = ngram[history]
-    x = random.random()
-    while True:
-        for c, v in dist.items():
-            x = x - v
-            if x <= 0:
-                return c
-
-
-def generate_text(ngram, order = 3, nletters = 1000):
-    history = '#' * (order - 1)
-    out = []
-    for i in xrange(nletters):
-        c = generate_letter(ngram, history, order)
-        history = history[-(order - 1) : ] + c
-        out.append(c)
-    return ''.join(out)
-
 
 if __name__ == "__main__":
     order = int(sys.argv[2])
     filename = sys.argv[1]
     model = Train(filename, order)
-
-    #for seq, char_prob in model.items():
-    #    if seq.count('#') > 0:
-    #        print seq + ' :'
-    #        for c, p in char_prob.items():
-    #            print '\t{0} / {1}'.format(c, p)
-
-    #print generate_text(model, order)
 
     print MakeFSA(model, order)
